be30d2083ff3acbc210eff78c51721b84ef9970d
Michael Lang <mwlang@apvwap01.(none)>
2009-11-11 15:33:23 -0500

    removed puts lines from sequel_output

--- lib/sequel_output.rb
+++ lib/sequel_output.rb
@@ -3,22 +3,24 @@ require 'db_output'
 class SequelOutput < BaseOutput
   attr_accessor :db_handle
   attr_accessor :unique_id
+  attr_accessor :jobs_table
 
-  def self.prepare_database(db_handle)
-    ensure_progress_table_exists(db_handle)
+  def self.prepare_database(db_handle, jobs_table = :progress_bars)
+    ensure_progress_table_exists(db_handle, jobs_table)
   end
   
-  def initialize(unique_id, db_handle)
-    puts "SequelOutput: initialize"
+  def initialize(unique_id, db_handle, jobs_table = :progress_bars)
+    @jobs_table = jobs_table
+    # puts "SequelOutput: initialize"
     @unique_id = unique_id
     @dbh = db_handle
     create_progress_bar
   end
   
   def post(progress_text, percentage, finished)
-    puts "SequelOutput: post('...', #{percentage}, #{finished})"
+    # puts "SequelOutput: post('...', #{percentage}, #{finished})"
     end_time = finished ? Time.now : nil
-    @dbh[:progress_bars].filter(:id => @unique_id).
+    @dbh[@jobs_table].filter(:id => @unique_id).
       update(:progress_text => progress_text, 
         :progress_percent => percentage,
         :last_updated => Time.now,
@@ -26,30 +28,30 @@ class SequelOutput < BaseOutput
   end
 
   def progress_text
-    puts "SequelOutput: progress_text"
-    @dbh[:progress_bars].filter(:id => @unique_id).
+    # puts "SequelOutput: progress_text"
+    @dbh[@jobs_table].filter(:id => @unique_id).
       select(:progress_text).first[:progress_text]
   end
   
   def progress_percent
-    puts "SequelOutput: progress_percent"
-    @dbh[:progress_bars].filter(:id => @unique_id).
+    # puts "SequelOutput: progress_percent"
+    @dbh[@jobs_table].filter(:id => @unique_id).
       select(:progress_percent).first[:progress_percent]
   end
   
   private
   
   def create_progress_bar
-    puts "SequelOutput: create_progress_bar"
-    if @dbh[:progress_bars].filter(:id => @unique_id).first
-      @dbh[:progress_bars].filter(:id => @unique_id).
+    # puts "SequelOutput: create_progress_bar"
+    if @dbh[@jobs_table].filter(:id => @unique_id).first
+      @dbh[@jobs_table].filter(:id => @unique_id).
         update(:progress_text => '', 
           :progress_percent => 0,
           :job_start => Time.now,
           :last_updated => Time.now,
           :job_finish => nil)
     else
-      @dbh[:progress_bars].
+      @dbh[@jobs_table].
         insert(:id => @unique_id, 
           :progress_text => '', 
           :job_start => Time.now, 
@@ -57,10 +59,10 @@ class SequelOutput < BaseOutput
     end
   end
   
-  def self.ensure_progress_table_exists(db_handle)
-    puts "SequelOutput: ensure_progress_table_exists"
-    return true if db_handle.tables.include?(:progress_bars)
-    db_handle.create_table(:progress_bars) do
+  def self.ensure_progress_table_exists(db_handle, jobs_table)
+    # puts "SequelOutput: ensure_progress_table_exists"
+    return true if db_handle.table_exists?(jobs_table)
+    db_handle.create_table(jobs_table) do
       primary_key :id
       String :progress_text
       Integer :progress_percent, :size => 3
@@ -68,6 +70,6 @@ class SequelOutput < BaseOutput
       DateTime :last_updated
       DateTime :job_finish
     end
-    puts "SequelOutput: ensure_progress_table_exists created the 'progress_bars' table."
+    puts "SequelOutput: ensure_progress_table_exists created the '#{jobs_table}' table."
   end
 end
